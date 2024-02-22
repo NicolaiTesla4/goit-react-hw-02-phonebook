6ac8231821c81b269ad4661eea528b84c5f438a6
Nicolai <134018297+NicolaiTesla4@users.noreply.github.com>
2024-02-21 22:40:29 -0500

    Twenty six

--- src/components/ContactList.jsx
+++ src/components/ContactList.jsx
@@ -8,7 +8,7 @@ const ContactList = ({ contacts, onDeleteContact }) => {
       {contacts.map(contact => (
         <li key={contact.id}>
           {contact.name}: {contact.number}
-          <button onClick={() => onDeleteContact(contact.id)}>Delete</button>
+          <button onClick={() => onDeleteContact(contact.id)} className={css.button}>Delete</button>
         </li>
       ))}
     </ul>
